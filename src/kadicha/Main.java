e418dead21f8900cef4e32e13bbe15a29ca915fb
KadichaKadicha <kadichatashieva@mail.ru>
2019-08-22 13:33:36 +0600

    Initial commit

---
+++ src/kadicha/Main.java
@@ -0,0 +1,20 @@
+package kadicha;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        System.out.println("ПРИВЕТ МИР !");
+        int возраст = 17;
+        System.out.println(возраст);
+        String pony = "baby";
+        String grown = "man";
+        System.out.println(pony + " " + grown);
+        final String привет = "pink";
+        System.out.println(привет);
+
+
+    }
+}
+
